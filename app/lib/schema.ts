60d64e0191152640570f0101479c4e78acbbc72a
Camilla <camillamdalan@gmail.com>
2024-03-12 16:15:51 +0100

    updates wrangler and org imports

--- app/lib/schema.ts
+++ app/lib/schema.ts
@@ -1,4 +1,5 @@
-import { InferInsertModel, InferSelectModel, relations } from "drizzle-orm";
+import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import {
   customType,
   foreignKey,
@@ -9,7 +10,7 @@ import {
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 import { createInsertSchema } from "drizzle-zod";
-import { BalanceSchema } from "generated-sources/gocardless";
+import type { BalanceSchema } from "generated-sources/gocardless";
 import { z } from "zod";
 
 export const user = sqliteTable(
