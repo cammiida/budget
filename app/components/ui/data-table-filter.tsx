b1f2fe6c6cb08803e1f90f973b4e59b7b0c8242c
Camilla <camillamdalan@gmail.com>
2024-03-19 14:03:35 +0100

    switches out button for div in popovertrigger to avoid button in button

--- app/components/ui/data-table-filter.tsx
+++ app/components/ui/data-table-filter.tsx
@@ -1,15 +1,14 @@
 import { ListFilter } from "lucide-react";
 import type { PropsWithChildren } from "react";
-import { Button } from "./button";
 import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 export function DataTableFilter({ children }: PropsWithChildren) {
   return (
     <Popover>
       <PopoverTrigger>
-        <Button className="h-8 w-8 p-0 lg:flex" variant="outline">
-          <ListFilter className="h-4 w-4" />
-        </Button>
+        <div className="inline-flex h-8 w-8 items-center justify-center rounded-md border border-input p-0 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+          <ListFilter className=" h-4 w-4 " />
+        </div>
       </PopoverTrigger>
       <PopoverContent>{children}</PopoverContent>
     </Popover>
